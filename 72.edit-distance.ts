407b04f70400d8aa37192dc8c4857c6a1c28cf4d
Atikur-Rahman-Sabuj <sabuj.kucse@gmail.com>
2022-06-09 10:32:46 +0600

    [72] Edit Distance

---
+++ 72.edit-distance.ts
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=72 lang=typescript
+ *
+ * [72] Edit Distance
+ */
+
+// @lc code=start
+function minDistance(word1: string, word2: string): number {
+   const dp: number[][] = [];
+   dp[0] = [];
+   for (let i = 0; i <= word1.length; i++) {
+      dp[i] = [];
+      dp[i][0] = i;
+   }
+   for (let i = 0; i <= word2.length; i++) {
+      dp[0][i] = i;
+   }
+   for (let i = 0; i <= word1.length; i++) {
+      {
+         for (let j = 0; j < word2.length; j++) {
+            if (word1[i] === word2[j]) {
+               dp[i + 1][j + 1] = dp[i][j];
+            } else {
+               dp[i + 1][j + 1] = Math.min(dp[i][j], Math.min(dp[i][j + 1], dp[i + 1][j])) + 1;
+            }
+         }
+      }
+   }
+   console.log(dp);
+   return dp[word1.length][word2.length];
+}
+// @lc code=end
