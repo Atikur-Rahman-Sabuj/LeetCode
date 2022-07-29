aac001f0640c9472d1bc30da95cf6e6d91cd8927
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-07-29 11:42:14 +0600

    Time: 104 ms (52.83%), Space: 44.3 MB (89.81%) - LeetHub

---
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+   let dummy = new ListNode(0);
+   dummy.next = head;
+   let slow = dummy;
+   let fast = dummy;
+   for (let i = 0; i < n; i++) {
+      fast = fast.next;
+   }
+   while (fast.next) {
+      slow = slow.next;
+      fast = fast.next;
+   }
+   slow.next = slow.next!.next;
+   return dummy.next;
+};
