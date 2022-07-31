d15a531346bb5332a33c3d2a4c052c07c2ec0204
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-07-31 23:13:46 +0600

    Time: 4246 ms (16.67%), Space: 99.1 MB (50.00%) - LeetHub

---
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.ts
@@ -0,0 +1,25 @@
+class NumArray {
+    nums;
+    constructor(nums: number[]) {
+        this.nums = nums;
+    }
+
+    update(index: number, val: number): void {
+        this.nums[index] = val;
+    }
+
+    sumRange(left: number, right: number): number {
+        let sum = 0;
+        for(let i = left ; i<=right ; i++){
+            sum += this.nums[i];
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * obj.update(index,val)
+ * var param_2 = obj.sumRange(left,right)
+ */
