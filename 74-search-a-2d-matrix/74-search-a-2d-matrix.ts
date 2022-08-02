85fb898561677729d2016fa20563791e966d9fbd
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-08-02 22:38:14 +0600

    Time: 93 ms (63.87%), Space: 45.1 MB (7.23%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.ts
@@ -0,0 +1,26 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+    const m = matrix.length, n = matrix[0].length;
+    if(m*n === 1) return matrix[0][0] === target;
+    let low = 0, high = m*n - 1;
+    while(low <= high){
+        //console.log(low, high);
+        let mid = Math.floor((low + high) / 2);
+        let midI = valueToIndex(mid);
+        //console.log(mid, midI)
+        if(target === matrix[midI[0]][midI[1]] ) return true;
+        if(target < matrix[midI[0]][midI[1]]){
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+    
+    function valueToIndex (value) {
+        const i = Math.floor(value / n);
+        const j = value % n;
+        return [i,j];
+    }
+    
+    return false;
+    
+};
