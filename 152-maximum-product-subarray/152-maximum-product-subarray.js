cf8fcf02a139a13e7c2e1f08dcc4ee62da02d112
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-08-09 00:11:56 +0600

    Time: 95 ms (55.17%), Space: 42.7 MB (54.26%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+   let pastMin = nums[0];
+   let pastMax = nums[0];
+   let maxProduct = nums[0];
+   let tempMin, tempMax;
+   for (let i = 1; i < nums.length; i++) {
+      tempMin = pastMin;
+      tempMax = pastMax;
+      pastMin = Math.min(tempMin * nums[i], tempMax * nums[i], nums[i]);
+      pastMax = Math.max(tempMin * nums[i], tempMax * nums[i], nums[i]);
+      maxProduct = Math.max(maxProduct, pastMin, pastMax);
+   }
+   return maxProduct;
+};
