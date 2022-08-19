5c300024e3d617ff44a49b89df565d6a6fc9a16b
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-08-19 22:54:18 +0600

    Time: 365 ms (46.79%), Space: 59.4 MB (47.86%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.js
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.js
@@ -3,8 +3,10 @@
  * @return {number}
  */
 var longestPalindrome = function(words) {
+    
     const m = new Map();
     let ans = 0;
+    
     for(word of words){
         const temp = word[1]+word[0];
         if(m.has(temp) && m.get(temp) > 0){
@@ -14,11 +16,13 @@ var longestPalindrome = function(words) {
             m.set(word, m.has(word)?m.get(word)+1:1);
         }
     }
+    
     for(word of m.keys()){
         if (word[0] === word[1] && m.get(word)>0 ){
             ans += 2;
             break;
         }
     }
+    
     return ans;
 };
