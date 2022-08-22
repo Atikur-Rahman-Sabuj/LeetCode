f2be1e6178bb99cacc2c60589bcb0bce995584d0
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-08-22 11:30:22 +0600

    Time: 101 ms (42.48%), Space: 42.6 MB (88.03%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
@@ -5,20 +5,22 @@
  */
 var nextGreatestLetter = function(letters, target) {
     const n = letters.length;
+    
+    //handle case for out of array
     if(target < letters[0]) return letters[0];
     if(target >= letters[n-1]) return letters[0];
+    
+    //simple binary search to find the character
     let low = 0, high = n-1, mid;
     while(low<=high){
         mid = Math.floor((low+high) / 2);
-        //console.log(mid)
         if(target < letters[mid] && target >= letters[mid-1]){
-            return letters [mid];
+            return letters[mid];
         }
-        if(target>=letters[mid]){
-            low = mid+1;
+        if(target >= letters[mid]){
+            low = mid + 1;
         }else{
             high = mid;
         }
-        console.log(low, high)
     }
 };
