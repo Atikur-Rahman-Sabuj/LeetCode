9ce2f651f02605d4289831f99c8767dd3264e272
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-08-23 09:27:15 +0600

    Time: 104 ms (60.70%), Space: 44.6 MB (42.98%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
@@ -12,21 +12,21 @@
  */
 var sortedArrayToBST = function(nums) {
     function createBST(left, right){
-    let mid = Math.floor((left+right)/2);
-    if(nums[mid] === undefined){
-        return null;
-    }
-    let node = {
-        val: nums[mid],
-        left:null,
-        right:null,
-    }
-    nums[mid] = undefined;
-    let leftN = createBST(left, mid - 1);
-    let rightN = createBST(mid+1, right);
-    node.left = leftN;
-    node.right = rightN;
-    return node;
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid] === undefined){
+            return null;
+        }
+        let node = {
+            val: nums[mid],
+            left:null,
+            right:null,
+        }
+        nums[mid] = undefined;
+        let leftN = createBST(left, mid - 1);
+        let rightN = createBST(mid+1, right);
+        node.left = leftN;
+        node.right = rightN;
+        return node;
     }
     return createBST(0, nums.length - 1);
 };
