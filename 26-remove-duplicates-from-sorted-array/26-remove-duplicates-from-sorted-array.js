fbd86858ff3b7c470c78334a0a7157a28bac6d98
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-08-23 15:32:45 +0600

    Time: 95 ms (79.55%), Space: 44.7 MB (50.96%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let index = 1;
+    for(let i = 1 ; i < nums.length ; i++){
+        if(nums[i] !== nums[i-1]){
+            nums[index++] = nums[i];
+        }
+    }
+    return index;
+};
