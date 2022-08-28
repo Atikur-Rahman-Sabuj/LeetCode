2e372d24bbc6339e0ce65b07e70858626ce2095e
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-08-28 10:23:40 +0600

    Time: 131 ms (100.00%), Space: 46.9 MB (100.00%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.js
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.js
@@ -5,9 +5,20 @@
  */
 var answerQueries = function(nums, queries) {
     const n = nums.length;
+    
+    //sort the nums first becasue we need to add numbers in increasing order
+    //to get longest length of sum
     nums.sort((a, b) => a - b);
+    
+    //create new 2d array from query to store order of the queries
+    //to find the order later after sorting
     const queryArray = queries.map((a, i) => [a, i, n]);
+    
+    //sort the array according to query value
     queryArray.sort((a, b) => a[0] - b[0]);
+    
+    //now calculate some and check with each qeury value
+    //and store the answe in third index of queryArray
     let ansI = 0;
     let sum = 0;
     for(let i = 0 ; i < n ; i++){
@@ -20,8 +31,10 @@ var answerQueries = function(nums, queries) {
         if(ansI >= queries.length) break;
     }
 
+    //sort queryArry again according to index to get them in order
     queryArray.sort((a, b) => a[1] - b[1]);
     
+    //now return the answer
     return queryArray.map(a => a[2]);
     
 };
