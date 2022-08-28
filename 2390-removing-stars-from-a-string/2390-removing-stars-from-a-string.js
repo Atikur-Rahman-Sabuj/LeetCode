8dcf94019c7d0878b60a5e2b0a7eaa0c3f354618
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-08-28 10:28:03 +0600

    Time: 123 ms (100.00%), Space: 57 MB (100.00%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+    let ans = [];
+    let sCount = 0;
+    //loop in reverse order keep count of the stars
+    //Only take a character when stars count is zero
+    for(let i = s.length - 1 ; i >= 0 ; i--){
+        if(s[i] === "*"){
+            sCount++;
+        }else{
+            if(sCount > 0) sCount--;
+            else {
+                ans.push(s[i]);
+            }
+        }
+        
+    }
+    
+    //reverse the array
+    ans.reverse();
+    //join to make a string and return
+    return ans.join("");
+};
