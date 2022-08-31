e139f808b005479f7782ad2c796d32bc188cb40d
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-08-31 23:56:23 +0600

    Time: 132 ms (5.14%), Space: 41.4 MB (96.54%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -6,13 +6,15 @@ var rob = function(nums) {
     if (nums.length === 1) {
       return nums[0];
    }
-   let dp = [nums[0], nums[1]];
-   for (let index = 2; index < nums.length; index++) {
-      let max = Number.MIN_SAFE_INTEGER;
-      for (let j = 0; j < index - 1; j++) {
-         max = Math.max(max, dp[j] + nums[index]);
-      }
+   let dp = [nums[0], nums[1], nums[2]+nums[0]];
+   for (let index = 3; index < nums.length; index++) {
+      //let max = Number.MIN_SAFE_INTEGER;
+      // for (let j = 0; j < index - 1; j++) {
+      //    max = Math.max(max, dp[j] + nums[index], dp[j] + nums[index]);
+      // }
+      let max = Math.max(dp[index-2] + nums[index], dp[index-3] + nums[index])
       dp[index] = max;
    }
+    //console.log(dp)
    return Math.max(dp[nums.length - 1], dp[nums.length - 2]);
 };
