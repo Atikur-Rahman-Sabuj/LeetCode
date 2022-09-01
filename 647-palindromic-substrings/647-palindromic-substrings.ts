f207feec150ab973aa0558dceb0b313b9312175b
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-09-02 00:52:52 +0600

    Time: 1038 ms (5.95%), Space: 48.4 MB (16.67%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.ts
@@ -0,0 +1,22 @@
+function countSubstrings(s: string): number {
+    const isPalindrome = (s: string) => {
+        let left = 0, right = s.length - 1;
+        while (left < right) {
+            if (s[left] !== s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        for (let j = i; j < s.length; j++) {
+            if (isPalindrome(s.slice(i, j + 1))) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
