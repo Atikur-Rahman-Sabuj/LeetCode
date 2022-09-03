477d9eb187e690b56a32ac19037113649436a702
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-09-03 23:33:55 +0600

    Time: 151 ms (74.78%), Space: 44.9 MB (92.31%) - LeetHub

---
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number[][]} stones
+ * @return {number}
+ */
+var removeStones = function(stones) {
+
+    let subsetCount = stones.length;
+    const parentMap = [];
+    
+    // Initialize the parent map to give each stone it's own set
+    for (let i = 0; i < stones.length; i++) {
+        parentMap[i] = i;
+    }
+    
+    for (let thisStonesIdx = 1; thisStonesIdx < stones.length; thisStonesIdx++) {           
+        const thisStone = stones[thisStonesIdx];
+        
+        for (let thatStonesIdx = 0; thatStonesIdx < thisStonesIdx; thatStonesIdx++) {
+            const thatStone = stones[thatStonesIdx];
+            
+            // Not in the same row or column, skip ahead
+            if (thisStone[0] !== thatStone[0] && thisStone[1] !== thatStone[1]) continue;
+            
+            // If this stone isn't already part of an existing subset
+            if (parentMap[thisStonesIdx] === thisStonesIdx) {
+                // Add it to that one's subset
+                parentMap[thisStonesIdx] = thatStonesIdx;
+                subsetCount -= 1;
+            } else {
+                // Find this stone's root
+                let currentThisStonesParentIndex = parentMap[thisStonesIdx];
+                while (parentMap[currentThisStonesParentIndex] !== currentThisStonesParentIndex) {
+                    currentThisStonesParentIndex = parentMap[currentThisStonesParentIndex];
+                }
+                
+                // Find that stone's root
+                let currentThatStonesParentIndex = parentMap[thatStonesIdx];
+                while (parentMap[currentThatStonesParentIndex] !== currentThatStonesParentIndex) {
+                    currentThatStonesParentIndex = parentMap[currentThatStonesParentIndex];
+                }
+
+                // If they're not in the same subset, merge them
+                if (currentThisStonesParentIndex != currentThatStonesParentIndex) {
+                    parentMap[currentThisStonesParentIndex] = currentThatStonesParentIndex;  
+                    subsetCount -= 1;
+                }
+            }
+        }
+    }
+    
+    return stones.length - subsetCount;
+};
