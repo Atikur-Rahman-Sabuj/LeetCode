cd69921bac35b7f4bf05d65ffc4be3ac76d21fb7
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-09-03 23:00:42 +0600

    Time: 88 ms (100.00%), Space: 42.3 MB (100.00%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.js
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.js
@@ -4,27 +4,6 @@
  */
 var isStrictlyPalindromic = function(n) {
     
-    if(!isPalindrome(n)) return false;
-    
-    for(let i = 2 ; i <= 36 ; i++){
-        if(!isPalindrome(ConvertBase(n, i))) return false;
-    }
-    
-    return true;
-    
-    function ConvertBase (num, baseTo) {
-        return num.toString(baseTo);
-    };
-    
-    function isPalindrome(num){
-        const sNum = num.toString();
-        const n = sNum.length;
-        for(let i = 0 ; i < Math.floor(n/2) ; i++){
-            if(sNum[i] !== sNum[n-i-1]){
-                return false;
-            }
-        }
-        return true;
-    }
+    return false;
     
 };
