0ca9aa4f6e9fc9584fa0571e5cbf25c001ab886f
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-09-10 01:57:11 +0600

    Time: 532 ms (20.00%), Space: 91.3 MB (20.00%) - LeetHub

---
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.ts
@@ -0,0 +1,24 @@
+function kSmallestPairs(nums1: number[], nums2: number[], k: number): number[][] {
+    const minHeap = new MinPriorityQueue({ priority: x => x[0] });
+    for (let i = 0; i < nums1.length; i++) {
+        const num1 = nums1[i];
+        const num2 = nums2[0];
+        
+        minHeap.enqueue([num1 + num2, i, 0]);
+    }
+    const n = nums2.length;
+    const res = [];
+    while (k > 0 && !minHeap.isEmpty()) {
+        const [sum, idx1, idx2] = minHeap.dequeue().element;
+        
+        res.push([nums1[idx1], nums2[idx2]]);
+        
+        if (res.length === k) return res;
+        
+        if (idx2 < n - 1) {
+            minHeap.enqueue([nums1[idx1] + nums2[idx2 + 1], idx1, idx2 + 1]);
+        } 
+    }
+    
+    return res;
+};
