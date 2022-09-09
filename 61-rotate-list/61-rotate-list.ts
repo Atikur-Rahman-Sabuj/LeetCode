b83c6cc4b4851d1f0edca5e203fd61178e830815
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-09-10 00:30:14 +0600

    Time: 146 ms (12.50%), Space: 44.7 MB (75.00%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function rotateRight(head: ListNode | null, k: number): ListNode | null {
+    if(head === null) return head;
+    let length = 0;
+    let temp = head;
+    while(temp !== null){
+        temp = temp.next;
+        length++;
+    }
+    k = k % length;
+    let slow = head;
+    let fast = head;
+    let count = 0;
+    while(fast.next !== null){
+        fast = fast.next;
+        count++;
+        if(count > k){
+            slow = slow.next;
+        }
+    }
+    //console.log(fast.val, slow.val);
+    fast.next = head;
+    const result = slow.next;
+    slow.next = null
+    return result;
+};
