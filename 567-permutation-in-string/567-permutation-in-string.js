a919c533ae84d3a9e2ef286602a675f2d67cf0ce
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-09-14 20:25:36 +0600

    Time: 10179 ms (5.01%), Space: 48.9 MB (31.03%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+      const checkPartial = (s1, s2) => {
+      //console.log(s1, s2);
+      const map = new Map();
+      for (const c of s1) {
+         if (map.has(c)) {
+            map.set(c, map.get(c) + 1);
+         } else {
+            map.set(c, 1);
+         }
+      }
+      let count = 0;
+      for (const c of s2) {
+         if (map.has(c)) {
+            map.set(c, map.get(c) - 1);
+            if (map.get(c) === 0) {
+               map.delete(c);
+            }
+            count++;
+         } else {
+            count--;
+         }
+         if (count === s1.length) {
+            return true;
+         }
+      }
+      return false;
+   };
+   for (let i = 0, j = i + s1.length; i < s2.length - s1.length + 1; i++, j++) {
+      if (checkPartial(s1, s2.substring(i, j))) {
+         return true;
+      }
+   }
+   return false;
+};
