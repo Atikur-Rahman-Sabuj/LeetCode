03f5aa929075638c33374be4acde722c98a7c94c
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-09-15 10:58:56 +0600

    Time: 739 ms (29.69%), Space: 73.5 MB (68.75%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} changed
+ * @return {number[]}
+ */
+var findOriginalArray = function(changed) {
+    const n = changed.length;
+    if(n & 1) return[];
+    changed.sort((a, b) => b - a);
+    const m = new Map();
+    const ans = [];
+    for(let i = 0 ; i < n ; i++){
+        let sq = changed[i] * 2;
+        if(m.has(sq) && m.get(sq) >= 1){
+            m.set(sq, m.get(sq) - 1);
+            ans.push(changed[i]);
+        }else{
+            if(m.has(changed[i])){
+                m.set(changed[i], m.get(changed[i]) + 1);
+            }else{
+                m.set(changed[i], 1);
+            }
+        }
+        //console.log(m)
+    }
+    //console.log(changed)
+    //console.log(ans)
+    return ans.length === Math.floor(n/2)? ans:[];
+};
