96b4804ce798d297d3bcc9a83b4394e02675872a
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-09-20 17:17:04 +0600

    Time: 153 ms (40.68%), Space: 48.4 MB (79.66%) - LeetHub

---
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.ts
@@ -0,0 +1,12 @@
+function numSubarrayProductLessThanK(nums: number[], k: number): number {
+    let ans = 0, s = 0, mul = 1 , t;
+    for(let i = 0 ; i < nums.length ; i++){
+        mul = mul * nums[i];
+        while(mul >= k && s <= i){
+            mul = mul / nums[s];
+            s++;
+        }
+        ans = ans + i - s + 1;
+    }
+    return ans;
+};
