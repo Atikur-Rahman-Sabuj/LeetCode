6e9dd01573cc980be2019769f7c25821a94c6067
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-09-22 21:07:11 +0600

    Time: 1680 ms (67.74%), Space: 28.5 MB (77.42%) - LeetHub

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findMaximumXOR(self, nums):
+        answer = 0
+        for i in range(32)[::-1]:
+            answer <<= 1
+            prefixes = {num >> i for num in nums}
+            answer += any(answer^1 ^ p in prefixes for p in prefixes)
+        return answer
