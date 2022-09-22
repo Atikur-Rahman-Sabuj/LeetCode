b0a632ca9145b8cfa48062a052db0a2d50397b41
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-09-22 21:09:58 +0600

    Time: 2514 ms (16.67%), Space: 185 MB (16.67%) - LeetHub

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.ts
@@ -0,0 +1,42 @@
+function findMaximumXOR(nums: number[]): number {
+   class TNode {
+      children: TNode[] = [];
+   }
+
+   class Trie {
+      root: TNode = new TNode();
+      insert = (num: number) => {
+         let node = this.root;
+         for (let i = 31; i >= 0; i--) {
+            const bit = (num >> i) & 1;
+            if (!node.children[bit]) {
+               node.children[bit] = new TNode();
+            }
+            node = node.children[bit];
+         }
+      };
+      searchMax = (num: number) => {
+         let node = this.root;
+         let ans = 0;
+         for (let i = 31; i >= 0; i--) {
+            const bit = (num >> i) & 1;
+            if (node.children[bit ^ 1]) {
+               ans = ans ^ (1 << i);
+               node = node.children[bit ^ 1];
+            } else {
+               node = node.children[bit];
+            }
+         }
+         return ans;
+      };
+   }
+   const trie = new Trie();
+   for (const num of nums) {
+      trie.insert(num);
+   }
+   let max = 0;
+   for (const num of nums) {
+      max = Math.max(max, trie.searchMax(num));
+   }
+   return max;
+}
