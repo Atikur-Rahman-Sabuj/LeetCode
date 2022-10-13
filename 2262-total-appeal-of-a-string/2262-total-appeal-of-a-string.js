049768b360579c598cd7d9344f03579575ad92fc
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-10-13 22:32:15 +0600

    Time: 150 ms (57.85%), Space: 45.4 MB (50.41%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.js
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var appealSum = function(s) {
-    let ans = 0;
+    let ans = 0, total = 0;
     const last = new Array(26).fill(0);
     for(let i = 0 ; i < s.length ; i++){
-        last[s[i].charCodeAt(0) - 'a'.charCodeAt(0)] = i+1;
-        for(const l of last){
-            ans += l;
-        }
+        let charCode = s[i].charCodeAt(0) - 'a'.charCodeAt(0);
+        total = total - last[charCode] + i + 1;
+        last[charCode] = i+1;
+        ans += total;
     }
     return ans;
 };
