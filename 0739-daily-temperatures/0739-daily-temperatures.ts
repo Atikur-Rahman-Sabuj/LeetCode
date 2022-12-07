f5b22428704bad882f4561f27987b2e983a8d749
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-12-08 00:05:30 +0600

    Time: 452 ms (76.19%), Space: 64.7 MB (77.98%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.ts
@@ -0,0 +1,16 @@
+function dailyTemperatures(temperatures: number[]): number[] {
+    const n = temperatures.length, stack = [], ans = [];
+    for(let i = n-1 ; i >= 0 ; i--){
+        let temp = temperatures[i];
+        while(stack.length > 0 && temperatures[stack[stack.length-1]] <= temp){
+            stack.pop();
+        }
+        if(stack.length  === 0){
+            ans[i] = 0;
+        }else{
+            ans[i] = stack[stack.length-1] - i;
+        }
+        stack.push(i);
+    }
+    return ans;
+};
