b254174ae33e4f06d23778929f36c8186983066a
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-12-13 15:30:14 +0600

    Time: 118 ms (53.04%), Space: 43.4 MB (81.55%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -3,13 +3,17 @@
  * @return {number}
  */
 var addDigits = function(num) {
-    while(num > 9){
-        let temp = 0;
-        while(num > 0){
-            temp += num % 10;
-            num = ~~(num / 10);
-        }
-        num = temp;
-    }
-    return num;
+    // O(1) solution
+    return 1 + (num - 1) % 9;
+    
+    //loop solution
+    // while(num > 9){
+    //     let temp = 0;
+    //     while(num > 0){
+    //         temp += num % 10;
+    //         num = ~~(num / 10);
+    //     }
+    //     num = temp;
+    // }
+    // return num;
 };
