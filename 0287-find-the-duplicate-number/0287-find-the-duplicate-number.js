a524f8016ae3fc8a2f306af8b127e78614fcca64
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-12-17 23:58:59 +0600

    Time: 113 ms (86.08%), Space: 50.8 MB (57.80%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    
+    
+    //time -> n
+    //space -> 1
+    let len = nums.length;
+    for (const num of nums) {
+        let idx = Math.abs(num);
+        if (nums[idx] < 0) {
+            return idx;
+        }
+        nums[idx] = -nums[idx];
+    }
+    return len;
+    
+    
+    //time nlong
+    //space 1
+    // const len = nums.length;
+    // let low = 1, high = len - 1;
+    // while(low<high){
+    //     let mid = Math.floor((low+high)/2);
+    //     let cnt = 0;
+    //     for(let i = 0 ; i < len ; i++){
+    //         if(nums[i] <= mid) cnt++;
+    //     }
+    //     if(cnt>mid){
+    //         high = mid;
+    //     }else{
+    //         low = mid+1;
+    //     }
+    // }
+    // return low;
+};
