3c6521ad93c99cf8e383de8932e340bc06ffd0a6
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-12-18 00:11:53 +0600

    Time: 85 ms (91.84%), Space: 44.4 MB (91.17%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -0,0 +1,61 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+    let row0 = 1, col0 = 1;
+    // check if any items from first row has zero
+    for (let i = 0 ; i < n ; i++){
+        if(matrix[0][i] === 0) {
+            row0 = 0;
+            break;
+        }
+    }
+    
+    // check if any items from first col has zero
+    for (let i = 0 ; i < m ; i++){
+        if(matrix[i][0] === 0){ 
+            col0 = 0;
+            break;
+        }
+    }
+    
+    // set first row and first col to zero if a item is zero
+    for(let i = 1 ; i < m ; i++){
+        for(let j = 1 ; j < n ; j++){
+            if(matrix[i][j] === 0){
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    //then set zeros according to first row and col
+    for(let i = 1 ; i < m ; i++){
+        for(let j = 1 ; j < n ; j++){
+            if(matrix[i][0] === 0 || matrix[0][j] === 0){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    
+    // apply zeros for first row
+    for (let i = 0 ; i < n ; i++){
+        if(row0 === 0) {
+            matrix[0][i] = 0;
+        }
+    }
+    
+    // apply zeroes for first col
+    for (let i = 0 ; i < m ; i++){
+        if(col0 === 0){ 
+            matrix[i][0] = 0
+        }
+    }
+
+    
+    return matrix;
+    
+};
