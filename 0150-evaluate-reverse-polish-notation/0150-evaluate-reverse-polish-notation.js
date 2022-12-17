936a9f3eb5ea2d9f6b8c778823f6ef759aa60217
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-12-17 12:04:43 +0600

    Time: 71 ms (95.84%), Space: 46 MB (27.66%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -7,9 +7,11 @@ var evalRPN = function(tokens) {
     const operators = ["+", "-", "*", "/"];
     for(const token of tokens){
         if(operators.includes(token)){
+            // if operator execute and push result to stack
             let temp = eval(st.pop(), st.pop(), token);
             st.push(temp);
         }else{
+            // if number push to stack
             st.push(parseInt(token));
         }
     }
@@ -23,6 +25,7 @@ function eval(par1, par2, operator){
         case "*" : return par2 * par1
         case "/" :{
             let temp = par2 / par1
+            // truncate towards zero
             if(temp > 0){
                 return Math.floor(temp)
             }else{
