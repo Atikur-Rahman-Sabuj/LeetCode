c28192650ae0b596f615e578063a277ee06be1c5
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2022-12-19 09:32:20 +0600

    Time: 805 ms (56.93%), Space: 181.4 MB (16.87%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+    if(source === destination) return true;
+    const vt = new Map();
+    const st = [source];
+    const eg = new Map();
+    for(const edge of edges){
+        
+        if(eg.has(edge[0])){
+            eg.get(edge[0]).push(edge[1]);
+        }else{
+            eg.set(edge[0], [edge[1]]);
+        }
+        
+        if(eg.has(edge[1])){
+            eg.get(edge[1]).push(edge[0]);
+        }else{
+            eg.set(edge[1], [edge[0]]);
+        }
+    }
+    
+    while(st.length > 0){
+        const temp = st.pop();
+        vt.set(temp, 1);
+        const tNs = eg.get(temp) || [];
+        for(let i = 0 ; i < tNs.length ; i++){
+            if(!vt.has(tNs[i])){
+                if(tNs[i] === destination) return true
+                st.push(tNs[i]);
+            }
+        }
+    }
+    return false;
+    
+};
