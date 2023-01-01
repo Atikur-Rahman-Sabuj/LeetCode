6f4ed34a51f4a82a3c3d8579c9f5be2d54146611
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-01-02 02:12:00 +0600

    Time: 65 ms (94.95%), Space: 44.8 MB (58.59%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.ts
@@ -0,0 +1,18 @@
+function numDecodings(s: string): number {
+   const dp = new Array(s.length + 1).fill(0);
+   dp[0] = 1;
+   dp[1] = s[0] === "0" ? 0 : 1;
+   for (let i = 2; i <= s.length; i++) {
+      const one = s[i - 1];
+      const two = s[i - 2] + s[i - 1];
+      if (one >= "1" && one <= "9") {
+         dp[i] = dp[i - 1];
+      }
+      if (two >= "10" && two <= "26") {
+         dp[i] += dp[i - 2];
+      }
+   }
+   //console.log(dp);
+
+   return dp[s.length];
+};
