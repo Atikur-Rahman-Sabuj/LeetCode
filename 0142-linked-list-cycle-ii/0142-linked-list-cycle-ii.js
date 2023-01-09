e52e1f9a7a9f69af1823944368f01ed20bc64de9
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-01-09 23:04:59 +0600

    Time: 343 ms (5.05%), Space: 44.3 MB (95.65%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let node = head;
+   if(head === null) return head;
+   while (true) {
+      if (node.next === null) return null;
+      if(node.next === node) return node;
+      let t = head;
+      while (t !== node) {
+         if (t === node.next) {
+            return t;
+         }
+         t = t.next;
+      }
+      node = node.next;
+   }
+};
