76d1f0f63fa86e57bab69d699b228281ed8f5128
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-01-15 16:26:01 +0600

    Time: 114 ms (100.00%), Space: 63.9 MB (100.00%) - LeetHub

--- 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.js
+++ 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.js
@@ -7,7 +7,6 @@ var countGood = function(nums, k) {
     let  res = 0, count = 0;
     const freq = new Map();
     for(let i = 0, j = 0; j < nums.length; ++j){
-        //console.log(i, j)
         count += freq.get(nums[j]) || 0;
         freq.set(nums[j],(freq.get(nums[j]) || 0) + 1);
 
@@ -16,7 +15,6 @@ var countGood = function(nums, k) {
             k += freq.get(nums[i++]);
         }
         res += i;
-        //console.log(i , j, res)
     }
     return res;
 };
