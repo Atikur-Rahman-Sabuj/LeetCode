eebdd68d1b75a4d4daa8aa8272cc6574c2375c20
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-02-15 10:22:37 +0600

    Time: 107 ms (82.30%), Space: 46.4 MB (91.61%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
@@ -7,9 +7,7 @@ var addToArrayForm = function(num, k) {
     let i = num.length - 1;
     let c = 0;
     while(i >= 0 || k > 0 || c > 0){
-       // console.log(k%10, num[i], c, i)
         let temp = k%10 + (num[i]??0) + c;
-        //console.log( temp)
         k = Math.floor(k/10)
         if(i >= 0)
             num[i] = temp%10
