d76ae64939d0e30413fabf9a02bac940c9d6f141
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-02-21 14:04:39 +0600

    Time: 67 ms (68.09%), Space: 45.1 MB (30.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -6,14 +6,12 @@ var singleNonDuplicate = function(nums) {
     let low = 0, high = nums.length - 1;
     while(low < high){
         let mid = Math.floor((low+high)/2)
-        //console.log(mid)
         if((nums[mid] !== nums[mid+1]) && (nums[mid-1] !== nums[mid])){
             return nums[mid];
         }
         if(nums[mid-1] === nums[mid]){
             mid = mid - 1;
         }
-        //console.log(mid)
         if(mid & 1){
             high = mid;
         }else{
