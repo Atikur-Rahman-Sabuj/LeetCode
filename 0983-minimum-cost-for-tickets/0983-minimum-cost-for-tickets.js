36500db08683770e125537e0ca0f3479b9cdcf86
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-03-28 11:27:51 +0600

    Time: 74 ms (29.79%), Space: 43.9 MB (26.24%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} days
+ * @param {number[]} costs
+ * @return {number}
+ */
+var mincostTickets = function(days, costs) {
+    days.sort((a,b) => a - b)
+    const dp = new Array(days.length).fill(Infinity);
+    let j = 0;
+    let k = 0;
+    
+    const dayMin = Math.min(...costs)
+    const weekMin = Math.min(costs[1], costs[2])
+    dp[0] = dayMin;
+    for(let i = 1 ; i < days.length ; i++){
+        while(days[i] - days[j] > 6){
+            j++;
+        }
+        while(days[i] - days[k] > 29){
+            k++;
+        }
+        dp[i] = Math.min((dp[i-1] + dayMin), (dp[j-1] || 0)+weekMin, (dp[k-1] || 0)+costs[2]);
+    }
+    //console.log(dp)
+    return dp[days.length - 1];
+};
