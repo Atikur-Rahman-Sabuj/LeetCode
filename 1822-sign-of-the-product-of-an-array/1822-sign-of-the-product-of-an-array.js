4603004389cb22689f499c028c5b11873297bf8f
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-05-02 09:53:49 +0600

    Time: 66 ms (29.58%), Space: 44.6 MB (9.00%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -3,13 +3,10 @@
  * @return {number}
  */
 var arraySign = function(nums) {
-    let ans = 1;
+    let nCount = 0;
     for(const num of nums){
         if(num === 0) return 0;
-        let nC = ans === 1 ? 0 : 1;
-        if(num < 0) nC++;
-        if(nC & 1) ans = -1;
-        else ans = 1;
+        if(num < 0) nCount++;
     }
-    return ans;
+    return nCount & 1 ? -1 : 1;
 };
