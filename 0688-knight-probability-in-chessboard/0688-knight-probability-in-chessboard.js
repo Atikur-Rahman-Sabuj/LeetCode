184ba93152f74a0c993f7d729e917a96d66fefed
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-07-22 20:53:07 +0700

    Time: 123 ms (48.44%), Space: 46.5 MB (59.38%) - LeetHub

---
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @param {number} row
+ * @param {number} column
+ * @return {number}
+ */
+const knightProbability = (N, K, r, c) => {
+  const dirs = [[-2, -1], [-1, -2], [1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1]];
+  const dp = [...Array(K + 1)].map(() => [...Array(N)].map(() => Array(N).fill(0)));
+  dp[0][r][c] = 1;
+  for (let k = 1; k <= K; k++) {
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        for (const [dx, dy] of dirs) {
+          const x = i + dx;
+          const y = j + dy;
+          if (x >= 0 && x < N && y >= 0 && y < N) {
+            dp[k][i][j] += dp[k - 1][x][y] / 8;
+          }
+        }
+      }
+    }
+  }
+  let res = 0;
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      res += dp[K][i][j];
+    }
+  }
+  return res;
+};
