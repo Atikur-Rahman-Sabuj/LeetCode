0033b65274bdafed14e64440c94dd0d993878a3f
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-08-06 22:53:45 +0700

    Time: 61 ms (100.00%), Space: 44.1 MB (100.00%) - LeetHub

---
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @param {number} goal
+ * @param {number} k
+ * @return {number}
+ */
+var numMusicPlaylists = function(n, goal, k) {
+    const MOD = 1e9 + 7;
+    let dp = Array.from({length: 2}, () => new Array(n + 1).fill(0));
+    dp[0][0] = 1;
+
+    for (let i = 1; i <= goal; i++) {
+        dp[i%2][0] = 0;
+        for (let j = 1; j <= Math.min(i, n); j++) {
+            dp[i%2][j] = dp[(i - 1)%2][j - 1] * (n - (j - 1)) % MOD;
+            if (j > k)
+                dp[i%2][j] = (dp[i%2][j] + dp[(i - 1)%2][j] * (j - k)) % MOD;
+        }
+    }
+
+    return dp[goal%2][n];
+};
