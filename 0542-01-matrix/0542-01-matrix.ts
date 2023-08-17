899a560b7a969befbad80668cd137234998131f9
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-08-17 11:15:42 +0700

    Time: 131 ms (95.93%), Space: 51.5 MB (91.06%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.ts
@@ -0,0 +1,35 @@
+function updateMatrix(mat: number[][]): number[][] {
+  const output: number[][] = Array(mat.length);
+   const BIG_NUMBER = 1000000;
+   const m = mat.length,
+      n = mat[0].length;
+   for (let i = 0; i < m; i++) {
+      output[i] = Array(n).fill(0);
+      for (let j = 0; j < mat[i].length; j++) {
+         if (i === 0 || j === 0) {
+            output[i][j] = mat[i][j] ? BIG_NUMBER : 0;
+         } else {
+            output[i][j] = mat[i][j] ? Math.min(output[i - 1][j], output[i][j - 1]) + 1 : 0;
+         }
+      }
+   }
+   for (let i = n - 2; i >= 0; i--) {
+      output[m - 1][i] = Math.min(output[m - 1][i], output[m - 1][i + 1] + 1);
+   }
+   for (let i = m - 2; i >= 0; i--) {
+      output[i][n - 1] = Math.min(output[i][n - 1], output[i + 1][n - 1] + 1);
+   }
+   for (let i = m - 2; i >= 0; i--) {
+      for (let j = mat[i].length - 2; j >= 0; j--) {
+         output[i][j] = mat[i][j] ? Math.min(Math.min(output[i + 1][j], output[i][j + 1]) + 1, output[i][j]) : 0;
+      }
+   }
+    //console.log(output)
+   for (let i = 1; i < m; i++) {
+      output[i][0] = Math.min(output[i][0], output[i - 1][0] + 1);
+   }
+   for (let i = 1; i < n; i++) {
+      output[0][i] = Math.min(output[0][i], output[0][i - 1] + 1);
+   }
+   return output;
+};
