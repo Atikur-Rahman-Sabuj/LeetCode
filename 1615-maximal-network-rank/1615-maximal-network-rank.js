067bd56ac06fb14789346482c4ea0ab5854c3092
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-08-18 10:40:29 +0700

    Time: 105 ms (55.74%), Space: 53.6 MB (18.03%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @param {number[][]} roads
+ * @return {number}
+ */
+var maximalNetworkRank = function(n, roads) {
+    const c = new Map();
+    const freq = new Array(n).fill(0);
+    for(const l of roads){
+        l.sort((a,b) => a - b) 
+        c.set(l[0]+"-"+l[1], 1);
+        freq[l[0]]++;
+        freq[l[1]]++;
+    }
+    //console.log(c)
+    //console.log(freq)
+    let ans = 0;
+    for(let i = 0 ; i < n - 1 ; i++){
+        for(let j = i+1 ; j < n ; j++){
+            let hasC = c.has(i+"-"+j)?1:0
+            ans = Math.max(ans, freq[i]+freq[j]-hasC)
+        }
+    }
+    return ans;
+    
+    
+};
