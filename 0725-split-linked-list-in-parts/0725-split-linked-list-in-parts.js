34c1431dd6c3e03768d5c3878cc107c84dd883e1
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-09-06 13:02:26 +0700

    Time: 66 ms (42.42%), Space: 44.5 MB (39.39%) - LeetHub

---
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode[]}
+ */
+var splitListToParts = function(head, k) {
+    let c = 0 , t = head;
+    while(t !== null){
+        c++;
+        t = t.next;
+    }
+    t = head;
+    const ans = new Array(k).fill(null), perPart = Math.floor(c / k);
+    let fullPart = c % k;
+    console.log(fullPart)
+    for(let i = 0 ; i < k ; i++){
+        if(t != null){
+            ans[i] = t;
+            let temp = perPart - 1;
+            if(fullPart > 0){
+                fullPart--;
+                temp++;
+            }
+            while(temp-- && t !== null){
+                t = t.next;
+            }
+            let node = t;
+            if(t !== null){
+                t = t.next;
+                node.next = null;
+            }
+            
+        }
+        
+    }
+    return ans;
+};
