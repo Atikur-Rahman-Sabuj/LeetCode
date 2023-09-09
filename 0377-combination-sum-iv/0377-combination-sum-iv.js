f676642c3bc1d184e7a40b88ea5925d7ea95a5da
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-09-09 22:47:16 +0700

    Time: 57 ms (61.44%), Space: 42.9 MB (63.03%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function(nums, target) {
+    const dp = new Array(target + 1).fill(0);
+    dp[0] = 1;
+    for (let i = 1; i <= target; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i >= nums[j]) {
+                dp[i] += dp[i - nums[j]];
+            }
+        }
+    }
+    console.log(dp)
+    return dp[target];
+};
