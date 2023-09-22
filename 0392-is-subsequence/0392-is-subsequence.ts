9afad1d6bfa3bef011ddf85d1d4779cfbebd04d4
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-09-22 10:31:25 +0700

    Time: 59 ms (45.64%), Space: 42.4 MB (81.62%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.ts
@@ -0,0 +1,9 @@
+function isSubsequence(s: string, t: string): boolean {
+    let j = 0;
+    for (let i = 0; i < t.length; i++) {
+        if(t[i] === s[j]){
+            j++;
+        }
+    }
+    return j === s.length;
+};
