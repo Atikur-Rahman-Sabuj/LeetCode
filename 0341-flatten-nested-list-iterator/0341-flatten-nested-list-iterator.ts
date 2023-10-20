2247f977d3fa8dd892633075c32121a6a72eab20
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-10-20 11:52:24 +0700

    Time: 75 ms (80.56%), Space: 53.9 MB (69.44%) - LeetHub

---
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.ts
@@ -0,0 +1,74 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *     If value is provided, then it holds a single integer
+ *     Otherwise it holds an empty nested list
+ *     constructor(value?: number) {
+ *         ...
+ *     };
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     isInteger(): boolean {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     getInteger(): number | null {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a single integer equal to value.
+ *     setInteger(value: number) {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+ *     add(elem: NestedInteger) {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds,
+ *     or an empty list if this NestedInteger holds a single integer
+ *     getList(): NestedInteger[] {
+ *         ...
+ *     };
+ * };
+ */
+
+class NestedIterator {
+    list;
+    i;
+    constructor(nestedList: NestedInteger[]) {
+		this.list = [];
+        this.i = 0;
+        const flatted = (nl) => {
+            nl.forEach(val => {
+                if(val.isInteger()){
+                    this.list.push(val.getInteger());
+                }else{
+                    flatted(val.getList());
+                }
+            })
+        }
+        flatted(nestedList)
+        
+        //console.log(this.list)
+    }
+
+    hasNext(): boolean {
+		return this.list.length > this.i;
+    }
+
+	next(): number {
+		return this.list[this.i++];
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new NestedIterator(nestedList)
+ * var a: number[] = []
+ * while (obj.hasNext()) a.push(obj.next());
+ */
