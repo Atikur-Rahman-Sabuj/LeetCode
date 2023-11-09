cff78ffab7676a610207d224dc1a52812bfecc49
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-11-09 12:45:15 +0700

    Time: 71 ms (42.22%), Space: 46.2 MB (20.00%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.js
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var countHomogenous = function(s) {
-    const sum = [0], MODULO = 1e9 + 7;
-    for(let i = 1 ; i <= 100000 ; i++){
-        sum[i] = sum[i-1] + i;
-    }
+    const MODULO = 1e9 + 7;
+    // for(let i = 1 ; i <= 100000 ; i++){
+    //     sum[i] = sum[i-1] + i;
+    // }
     let freq = 0, lchar = '0', ans = 0;
     for(let i = 0 ; i <= s.length ; i++){
         if(lchar === s[i]){ 
             freq++;
         }else{
-            ans = (ans + sum[freq]) % MODULO;
+            ans = (ans + (((freq)*(freq+1))/2)) % MODULO;
             lchar = s[i];
             freq = 1;
         }
