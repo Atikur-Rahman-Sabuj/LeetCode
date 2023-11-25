25d0cc3257b86d025c19f3f463ed2689169df3ef
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-11-25 17:09:20 +0700

    Time: 63 ms (16.05%), Space: 41.9 MB (88.37%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+        let own = -prices[0], notOwn = 0,ans = 0;
+    for(let i = 1 ; i < prices.length ; i++){
+        let prevOwn = own, prvNotOwn = notOwn;
+        
+        own = Math.max(prevOwn, prvNotOwn - prices[i]);
+        notOwn = Math.max(prvNotOwn, prevOwn + prices[i]);
+        //console.log(own, notOwn)
+        ans = Math.max(ans, notOwn);
+    }
+    return ans;
+};
