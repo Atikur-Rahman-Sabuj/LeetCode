9eac86cf769025b73bbf86502124fe7942c4f449
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-12-19 02:11:47 +0700

    Time: 63 ms (13.81%), Space: 44.5 MB (19.37%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(head === null || head.next === null){
+        return head;
+    }
+    let listHead = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return listHead;
+};
