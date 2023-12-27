576c618ee8e9edf3bcb98fb68bd1f3e65c7815b4
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2023-12-27 12:05:21 +0700

    Time: 74 ms (91.84%), Space: 52.1 MB (95.92%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} colors
+ * @param {number[]} neededTime
+ * @return {number}
+ */
+var minCost = function(colors, neededTime) {
+    let lastMax = neededTime[0], totalCost = neededTime[0];
+    for(let i = 1 ; i < colors.length ; i++){
+        if(colors[i] == colors[i-1]){
+            totalCost += neededTime[i];
+            lastMax = Math.max(lastMax, neededTime[i]);
+        }else{
+            totalCost -= lastMax;
+            lastMax = neededTime[i];
+            totalCost += lastMax;
+        }
+    }
+    totalCost -= lastMax;
+    return totalCost;
+    
+};
