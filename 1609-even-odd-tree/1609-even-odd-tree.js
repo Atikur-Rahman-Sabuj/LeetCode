2e52a369a4ddb899e5a1cdbcffc8832b59cc78be
Md. Atikur Rahman Sabuj <sabuj.kucse@gmail.com>
2024-03-01 02:03:41 +0700

    Time: 376 ms (10.53%), Space: 97 MB (21.05%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isEvenOddTree = function(root) {
+    const check = [];
+    let ans = true;
+    traverse(root, 0)
+    function traverse(node, level){
+        //console.log(check, ans)
+        if(node == null) return;
+        traverse(node.left, level + 1);
+        if(check[level] == undefined){
+            if(((level & 1) && (node.val & 1)) || (!(level & 1) && !(node.val & 1))){
+                ans = false;
+                return;
+            }
+            check[level] = node.val;
+        }else{
+            if(level & 1){
+                if((node.val & 1) || node.val >= check[level]){
+                    ans = false;
+                    return;
+                }else{
+                    check[level] = node.val;
+                }
+            }else{
+                if(!(node.val & 1) || node.val <= check[level]){
+                    ans = false;
+                    return;
+                }else{
+                    check[level] = node.val;
+                }
+            }
+        }
+        traverse(node.right, level + 1);
+    }
+    return ans;
+};
